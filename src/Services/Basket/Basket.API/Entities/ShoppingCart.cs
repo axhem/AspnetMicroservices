a241fc9cd192f58e9819ea0fb9f3d03495fb7a2c
axhem <axhemshahaj@gmail.com>
2022-07-06 12:23:25 +0200

    Added Basket microservice, Repository and Controller

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+        
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public string Username { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+
+                return totalprice;
+            }
+        }
+
+        public ShoppingCartItem GetItem(string productId)
+        {
+            return Items.FirstOrDefault(x => x.ProductId == productId);
+        }
+        
+
+    }
+}
